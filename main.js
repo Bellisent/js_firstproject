96e1bafb12d68f509a2f6a83474a605c7af58048
Bellisent <vikashlapak@mail.ru>
2023-10-31 18:59:53 +0200

    Наш второй коммит

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
-console.log('hello world')
-console.log(888)
-console.log ('hello' + 'world');
+// // console.log('hello world')
+// // console.log(888)
+// // console.log ('hello' + 'world');
+
+// // alert("Наш первый алерт");
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// console.log(1000/0);
+// console.log(infinity);
+
+// console.log("text" - 1);
+
+// console.log(NaN ** 0);
+
+// console.log(1728273827283838272834344434n);
+
+// let str1 = 'Anime';
+// let str2 = "Naruto";
+// let str3 = `Anime ${str2}`;
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+
+
+// let check = true;
+// let offCheck = false;
+// console.log(4 < 1);
+
+
+// let nuller = null;
+// console.log(nuller);
+
+// let a;
+// console.log(a);
+
+// typeof a;
+
+// console.log(typeof 12345);
+// console.log(typeof 10n);
+// console.log(typeof false);
+// console.log(typeof "cat");
+// console.log(typeof "123");
+// console.log(typeof null);
+
+
+document.getElementById('first').innerHTML = 'Наш текст';
+
+
+console.log(7);
+console.log('Добро' + 'пожаловать' + 'на' + 'курс' + 'Виктория');
+alert ("31 октября 2023");
+
+
+
+
+
+
+
+
