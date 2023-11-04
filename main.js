7c6c5794d668756f4a4680d8fa57af84c5ae987b
Bellisent <vikashlapak@mail.ru>
2023-11-04 23:49:32 +0200

    Наш первый коммит

--- main.js
+++ main.js
@@ -48,17 +48,45 @@
 // console.log(typeof null);
 
 
-document.getElementById('first').innerHTML = 'Наш текст';
+// document.getElementById('first').innerHTML = 'Текст';
+// document.getElementById('first').innerHTML = 'Новый текст';
+
+// console.log(7);
+// console.log('Добро' + 'пожаловать' + 'на' + 'курс' + 'Виктория');
+// alert ("31 октября 2023");
 
 
-console.log(7);
-console.log('Добро' + 'пожаловать' + 'на' + 'курс' + 'Виктория');
-alert ("31 октября 2023");
+// let jojo = 'jojo'
+// jojo = 'joji';
+// console.log(jojo);
+
+// document.querySelector('#first').innerHTML='<h1> Текст, которого нет</h1>'
+
+// document.querySelector('p').innerHTML='Хакуна Матата'
+
+// let inputIN = document.querySelector(".text")
+// let btn = document.querySelector('.check')
+// let div = document.querySelector('.out')
+
+// btn.onclick = function(){
+//     let e = inputIN.value
+//    div.innerHTML += `${e}<br>`
+// }
 
 
+// let myhappy = prompt('Ты любишь Аниме');
+// console.log(myhappy);
+// // if(условие){
+// //     Действие, которое должно будет быть выполнено
+// // }
+// if(myhappy == 'Да'){
+//     div.innerHTML = '<h1>Welcome</h1>'
+// }else{
+//     div.innerHTML = '<h1>Тебе здесь не рады</h1>'
+// }
 
-
-
-
-
-
+let a = prompt('Назовите любое число')
+console.log(a);
+if(a > 100){
+    alert('Right!')
+}
