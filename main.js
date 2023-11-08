46a13a7924d77908d73f5a94f0149bab26e7b0bb
Bellisent <vikashlapak@mail.ru>
2023-11-08 10:50:37 +0200

    Наш первый коммит

--- main.js
+++ main.js
@@ -85,8 +85,96 @@
 //     div.innerHTML = '<h1>Тебе здесь не рады</h1>'
 // }
 
-let a = prompt('Назовите любое число')
-console.log(a);
-if(a > 100){
-    alert('Right!')
+// let a = prompt('Назовите любое число')
+// console.log(a);
+// if(a > 100){
+//     alert('Right!')
+// }
+
+
+// let year = prompt("В каком году вышел ходячий замок?");
+// if(year < 2004){
+//     console.log("Рано");
+// }
+// else if(year > 2004){
+//     console.log("Поздно");
+// }
+// else{
+//     console.log("Верно");
+// }
+
+// if(year < 2004){
+//     console.log("Рано");
+// }
+// if(year > 2004){
+//     console.log("Поздно");
+// }
+// if(year ==2004){
+//     console.log("Верно");
+// }
+
+// let day = prompt("Сколько дней в неделе?");
+// Вариант классический
+// let week;
+// console.log(week);
+// if(day == 7 ){
+//     week = true
+// } else {
+//     week = false;
+// }
+// console.log(week);
+// Вариант через тернарик
+// let week = day == 7 ? true : false;
+// console.log(week);
+// Макет тернаргого оператора 
+// let Результат = Условие ? Первый вариант : Второй вариант
+// let Результат = (Условие) ? Первый вариант : Второй вариант
+
+// Возведение в степень
+// console.log(2 **25);
+// Остаток от деления 
+// console.log(5 % 3);
+// ((Строка + числовое значение (слепка) * на число)
+// console.log(('1' + 1) * 2);
+
+
+// >< - операторы больше или меньше 
+// >= <= - операторы больше или равно и меньше или равно
+// == не строгое равно
+// === строгое равно 
+// != не равно 
+// console.log("lets" > "fils");
+
+// && Логический оператор И
+// let a = 10;
+
+
+// if(a < 9 && a > 5 && a == 10){
+//     console.log(a);
+// }
+
+// || Логический оператор ИЛИ
+// let a = 7;
+// if (a < 9 || a > 5 || a == 10){
+//     console.log(a);
+// }
+
+
+// let a = Number|prompt ("Введите переменную a");
+// let b = Number|prompt ("Введите переменную b")
+// let y = (a + b);
+// if (y > 15){
+//     console.log("Да");
+// }
+// if (y <= 15){
+//     console.log("Нет");
+// }
+
+let a = prompt ("Введите переменную a");
+let b = prompt ("Введите переменную b");
+if (a === b){
+    console.log(true);
+}
+if (a != b){
+    console.log(false);
 }
